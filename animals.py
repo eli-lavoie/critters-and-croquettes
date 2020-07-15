f66a2d497b2bfb66e1a71dc51f7c7877560d4333
Eli Lavoie <storey14@googlemail.comgit config --global user.name Eli>
2020-07-15 15:13:45 -0500

    completed book 1 chapter 10

---
+++ animals.py
@@ -0,0 +1,130 @@
+from datetime import date
+
+#Land Animals
+class Pangolin:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = True
+
+class Hedgehog:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = True
+
+class Echidna:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = True
+
+class Fox:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = True
+
+class Capybara:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = True
+
+#Swim Animals
+class Axolotl:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = True
+
+class Koi:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = True
+
+class Eel:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = True
+
+class Goldfish:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = True
+
+class Tuna:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = True
+
+#Slither Animals
+class Python:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.sliterhing = True
+
+class Anaconda:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.sliterhing = True
+
+class Mamba:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.sliterhing = True
+
+class Cobra:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.sliterhing = True
+
+class Taipan:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.sliterhing = True
+
+#Creation Time
+pango = Pangolin()
+sonic = Hedgehog()
+yusuke = Fox()
+knuckles = Echidna()
+captain_america = Capybara()
+
+axl_rose = Axolotl()
+kai = Koi()
+sparky = Eel()
+brains = Goldfish()
+riley = Tuna()
+
+jetbrain = Python()
+takamaki = Anaconda()
+number_five = Mamba()
+kai_but_funnier = Cobra()
+numba_one = Taipan()
+
+print(numba_one)
